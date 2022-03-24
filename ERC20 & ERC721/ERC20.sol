628daf52f4ce5b1c55b5a52605899ca81ee3c556
skengmane <Aboluwatife1@gmail.com>
2022-03-24 19:07:05 +0100

    Update ERC20.sol

--- ERC20 & ERC721/ERC20.sol
+++ ERC20 & ERC721/ERC20.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
+
+contract SkiCoins is ERC20, Ownable {
+    uint256 public rate = 1000 * 10 ** decimals();
+    constructor() ERC20("SkiCoins", "SKC") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function buyToken(address receiver) public payable returns(bool){
+        require(msg.value > 0, "InSufficientBalance");
+        uint amount = msg.value * 1000 * 18 ** decimals();
+        _mint(receiver, amount);
+        return true;
+    
+    }
+}   
